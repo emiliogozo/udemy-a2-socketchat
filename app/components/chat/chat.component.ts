5180cb6bc31e7385f2388ddb6c100317e4d2f03d
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-13 00:34:13 +0800

    feat: Added chat component and service

---
+++ app/components/chat/chat.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'chat',
+  templateUrl: 'chat.component.html',
+})
+export class ChatComponent  {
+  messages: any = [];
+  message: string;
+
+  constructor() { }
+
+  sendMessage() {
+    console.log(this.message);
+  }
+}
