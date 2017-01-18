1f49a8b8089d7feabbcfce6fb8d39c61f6fa7954
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-18 23:12:36 +0800

    feat: Message stream

--- app/components/chat/chat.component.ts
+++ app/components/chat/chat.component.ts
@@ -1,17 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ChatService } from '../../services/chat.service';
 
 @Component({
   moduleId: module.id,
   selector: 'chat',
   templateUrl: 'chat.component.html',
 })
-export class ChatComponent  {
+export class ChatComponent implements OnInit, OnDestroy {
   messages: any = [];
   message: string;
+  connection: any;
 
-  constructor() { }
+  constructor(private _chatService: ChatService) { }
 
   sendMessage() {
-    console.log(this.message);
+    this._chatService.sendMessage(this.message);
+    this.message = '';
+  }
+
+  ngOnInit() {
+    this.connection = this._chatService.getMessages().subscribe(message=> {
+      console.log(message);
+      this.messages.push(message);
+    })
+  }
+
+  ngOnDestroy() {
+    this.connection.unsubscribe();
   }
 }
